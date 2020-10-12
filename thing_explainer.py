49ffc9251f4c7e8ac89cea2462297e1f4827641f
caya <caya@askanna.io>
2020-10-12 12:56:11 +0200

    improved forget_and_explain

--- thing_explainer.py
+++ thing_explainer.py
@@ -4,23 +4,24 @@ import os
 from myProgram import *
 
 
-def forgetnext(pizzafeatures):
+def forget_next(pizza_features):
     """this function only adds items in a list to the signature file
         which wil be used to call the forgetting tool
     """
     sig = open("datasets/signature.txt","a") 
     sig.seek(0)
     sig.truncate() #clear the file
-    for feature in pizzafeatures:
+    for feature in pizza_features:
         sig.write("\nhttp://www.co-ode.org/ontologies/pizza/pizza.owl#") #add the hyperlink first
         sig.write(feature)
     sig.close()
 
-toforget = ["ChickenTopping", "PizzaBase"] #the things that will be added to the file
 
-forgetnext(toforget)
+to_forget = ["ChickenTopping", "PizzaBase"] #the things that will be added to the file
 
-#to make the subclassstatements more legible by removing the hyperlinks
+forget_next(to_forget)
+
+# to make the subclassstatements more legible by removing the hyperlinks
 # def simplify(dataset):
 #     f = open(dataset, "r")
 #     for line in f:
@@ -28,7 +29,7 @@ forgetnext(toforget)
 #         scr = scr.replace("http://www.co-ode.org/ontologies/pizza/pizza.owl#", "")
 #         scr = scr.replace("http://www.w3.org/2000/01/rdf-schema#", "")
 #         scr = scr.replace("http://www.w3.org/2000/01/rdf-schema#", "")
-#         #scr = scr.replace ("<subClassOf>", "is a")
+#         scr = scr.replace ("<subClassOf>", "is a")
 #
 #         print(scr)
 #
@@ -43,10 +44,12 @@ forgetnext(toforget)
 #forgetOntology = inputOntology
 def forget_and_explain(inputOntology, inputSubclassStatements, things_to_forget):
     method = '1'
-    for i in len(things_to_forget):
+    for i in things_to_forget:
         os.system('java -jar kr_functions.jar ' + 'printAllExplanations' + " " + inputOntology + " " + inputSubclassStatements)
-        forgetnext(things_to_forget[i])
+        forget_next(i)
         signature = "datasets/signature.txt"
-        os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + forgetOntology + ' --method ' + method  + ' --signature ' + signature)
+        os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + forgetOntology + ' --method ' + method + ' --signature ' + signature)
 
+
+forget_and_explain(inputOntology, inputSubclassStatements, to_forget)
 
